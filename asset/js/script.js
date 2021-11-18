2f3a8dbfe92bd12553af560d1dd9c89346ce8292
kimsanghee2021 <tkdgmltjddnr@naver.com>
2021-11-18 22:54:36 +0900

    메인 끝

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,12 +1,18 @@
 $(function(){
     var $firstMenu = $('.menu > ul > li');
     var $submenu = $('#header .submenu_bg');
-    var $submenuBg = $('#header:after');
+    var $hambarger = $('#header .hamMenu');
     $firstMenu.mouseover(function(){
         $submenu.stop().slideDown(200);
-        $submenuBg.stop().slideDown(200);
     }).mouseout(function(){
         $submenu.stop().slideUp(200);
-        $submenuBg.stop().slideUp(200);
+    });
+    $submenu.mouseover(function(){
+        $submenu.stop().slideDown(200);
+    }).mouseout(function(){
+        $submenu.stop().slideUp(200);
+    });
+    $hambarger.click(function(){
+        $submenu.toggle();
     });
 }); 
