af82db25d0e97ed05b72919a15b571e3ed47c93b
SangHeeKim <tkdgmltjddnr@naver.com>
2022-01-07 17:46:04 +0900

    제이슨 파일 연결 작업

--- test_contest/js/script.js
+++ test_contest/js/script.js
@@ -1,5 +1,34 @@
-$(function(){
-    $('.d_flex li a').click(function(){
-        $(this).parents('li').addClass('active').siblings().removeClass('active');
-    });
+//첫번째 해야할 일 
+//1. 상품을 먼저 제이쿼리 ui로 움직일 수 잇게 짜기 
+//2. 인풋박스에 텍스트 입력시 그 제품만 노출시키기 
+//3. 상품을 끌고가서 장바구니에 놓았을때 그 부분에 상품이 담게 하기 
+//4. 상품의 갯수를 추가하거나 뺴면 금액이 변경되게 하기 
+
+$(document).ready(function(){
+    start();
 });
+
+
+
+function start(){
+    var products = [];
+
+    //데이터 넣는 아작스 
+    getProducts();
+}
+
+function getProducts(){
+    $.ajax({
+        url : './json/store.json',
+        type: 'GET',
+    }).done(function(data){
+        data.products.forEach(function(상품 , i){
+            appendProduct(상품, i);
+        });
+    });
+}
+
+//상품을 넣는 코드 
+function appendProduct(product, index){
+
+}
