280cac56f7ffb1bdf595114711fe72a70384213f
kimsanghee <tkdgmltjddnr@naver.com>
2022-03-17 20:34:51 +0900

    exprot import 사용 방법

---
+++ scriptES6/import&export/library.js
@@ -0,0 +1,21 @@
+var a = 10;
+var b = 20;
+var c = 30;
+
+export {a,b};
+export default c;
+
+/*
+
+1.export 문법 
+-import 가져올거 from '경로'
+- export default 내보낼거
+
+하나만 내보내는 export default 문법
+- export default 내보낼거
+
+여러개를 내보내는 export 문법
+- export {내보낼것들}
+
+A as 별명 => A의 변수명을 별명으로 변경하는 방법
+*/
